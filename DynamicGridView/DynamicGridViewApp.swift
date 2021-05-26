a1d8b25b204d7c019b7d5aa2e06ede3b45d439c5
Jeongmin <tdcian71@gmail.com>
2021-05-26 23:23:21 +0900

    Initial Commit

---
+++ DynamicGridView/DynamicGridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicGridViewApp.swift
+//  DynamicGridView
+//
+//  Created by APPLE on 2021/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicGridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
